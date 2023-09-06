ea2863ee61f056bf3b8ff63006f090940506a127
LeonardoCapocci <leocap2001@gmail.com>
2023-09-06 13:58:52 -0700

    raindrops fall down the screen

---
+++ drop.py
@@ -0,0 +1,23 @@
+import pygame
+from pygame.sprite import Sprite
+
+class Drop(Sprite):
+    """A class to represent a single raindrop."""
+    def __init__(self, game):
+        """Initialize raindrop attributes and starting position."""
+        super().__init__()
+        self.screen = game.screen
+        self.screen_rect = self.screen.get_rect()
+
+        self.image = pygame.image.load('drop.bmp')
+        self.image = pygame.transform.scale(self.image, (60, 100))
+        self.rect = self.image.get_rect()
+
+        self.rect.x = 0
+        self.rect.y = 0#-self.rect.height
+
+        self.y = float(self.rect.y)
+
+    def update(self):
+        """Updates raindrop positions."""
+        self.rect.y += 5
--- raindrops.py
+++ raindrops.py
@@ -1,31 +1,59 @@
 import sys
 import pygame
 
-#from drop import Drop
+from drop import Drop
 
 class Raindrops:
-    """A class to represent raindrops falling down the sky to the ground"""
+    """A class to represent raindrops falling down the sky to the ground."""
     def __init__(self):
-        """Initialize starting app atributes"""
+        """Initialize starting app atributes."""
         pygame.init()
         self.screen = pygame.display.set_mode((1280,720))
+        self.screen_rect = self.screen.get_rect()
         self.clock = pygame.time.Clock()
         self.bg_color = (151, 204, 255)
+        self.drops = pygame.sprite.Group()
+
+        self.create_rain()
     
     def run_app(self):
-        """Main loop for the app"""
+        """Main loop for the app."""
         while True:
-            self.screen.fill(self.bg_color)
-            for event in pygame.event.get():
-                if event.type == pygame.QUIT:
-                    sys.exit()
-                if event.type == pygame.KEYDOWN:
-                    if event.key == pygame.K_q:
-                        sys.exit()
+            self._check_events()
+            self._update_drops()
+            self._update_screen()
 
             self.clock.tick(60)
             pygame.display.flip()
 
+    def create_rain(self):
+        """Creates a row of raindrops at the top of the screen."""
+        drop = Drop(self)
+        drop_width = drop.rect.width
+        current_x = drop_width * 0.5
+        while current_x < (self.screen_rect.width - drop_width):
+            new_drop = Drop(self)
+            new_drop.rect.x = current_x
+            current_x += 2 * drop_width
+            self.drops.add(new_drop)
+
+    def _update_drops(self):
+        """Update the drops."""
+        self.drops.update()
+
+    def _check_events(self):
+        """Checks keyboard events."""
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sys.exit()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_q:
+                    sys.exit()
+
+    def _update_screen(self):
+        """Updates screen for each loop."""
+        self.screen.fill(self.bg_color)
+        self.drops.draw(self.screen)
 
 if __name__ == '__main__':
     rd = Raindrops()
